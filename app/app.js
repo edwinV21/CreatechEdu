ec87333b4eaf1c0de9c60c9accd3e30f4fd96ef6
edwinv21 <edvl21@gmail.com>
2016-12-26 16:52:43 -0600

    added structure changes

--- app/app.js
+++ app/app.js
@@ -8,5 +8,9 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  //$routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+    .when("/", {redirectTo: '/view1'} );
+
+
 }]);
