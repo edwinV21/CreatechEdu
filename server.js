9dce696b8d6c94e33dcf539fe1ff8eb39397a183
edwinv21 <edvl21@gmail.com>
2017-01-19 14:40:23 -0600

    rest api update

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+
 app.use(express.static(__dirname + "/app"));
+
+var router = express.Router();
+
+router.get('/pruebaGet', function(req, res) {
+   res.send("Hello World!");
+});
+app.use(router);
+
+
 app.listen(process.env.PORT || 3000);
 console.log("Server up on port 3000");
