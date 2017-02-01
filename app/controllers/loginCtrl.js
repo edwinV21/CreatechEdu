60b99ff96c3adc2446ae9280d0c47946f466353f
edwinv21 <edvl21@gmail.com>
2017-01-31 21:03:56 -0600

    added database changes

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -4,8 +4,10 @@ module.exports = function(){
   var bodyParser = require('body-parser')
   var app = express();
   const pgp =require ('pg-promise')();
-  const db =pgp('postgres://:@localhost/educationdb' );
+  const env=process.env.DATABASE_URL;
+  const db =pgp(env || 'postgres://:@localhost/educationdb' );
   var fs = require("fs");
+
   app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
   app.use(bodyParser.json());
@@ -13,10 +15,11 @@ module.exports = function(){
   app.post('/', function(req, res){
       console.log("revisando");
       console.log(req.body);
-      res.send({ status: 'SUCCESS' });
       db.any('SELECT * FROM USERS',[true] ).then (function(data) {
       console.log('data',data[0] );
-      console.log(reviewUsr(3,4) );
+      res.send({ status: data[0] });
+
+      //console.log(reviewUsr(3,4) );
       } );
 
 
